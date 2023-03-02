b4e11269f4a310565a729fb14fc8afefba8ef63a
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-03-02 16:58:03 -0300

    starting project in NextJS

---
+++ pages/components/login/Login.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+function Login() {
+  return (
+    <div className='background-image'>
+        <Container>Texte</Container>
+    </div>
+  )
+}
+
+export default Login
---
+++ pages/components/login/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import bg from '../../../assets/images/background.webp'
+const Container = styled.div`
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url();
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`
+
+export { Container }
