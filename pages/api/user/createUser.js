863b75d22a3baf187edbdd1059a258716091e8ff
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-04-24 18:50:10 -0300

    fixing bugs

--- pages/api/user/createUser.js
+++ pages/api/user/createUser.js
@@ -2,13 +2,13 @@ import { PrismaClient } from "@prisma/client"
 import { NextApiRequest, NextApiResponse } from "next"
 const prisma = new PrismaClient()
 
-export default async function createUser(req: NextApiRequest, res: NextApiResponse) {
+export default async function createUser(req, res) {
     const { email, pass, name } = req.body
     if (!email || !pass || !name) res.status(404).json({ msg: "data can't be empty" })
     try {
       await prisma.user.create({ data: { email: email, password: pass, name: name } })
       res.status(201).json({ msg: "User created!" })
-    } catch (err: any) {
+    } catch (err) {
       res.status(500).json({ msg: err.message})
     } finally {
       async () => {
